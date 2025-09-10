87f17c8bef9bdbfb7ca9d328b10c11b15c091b63
GabeCasciano <gabecasciano@gmail.com>
2025-09-10 18:24:54 -0400

    added weather script

--- start_work.sh
+++ start_work.sh
@@ -18,6 +18,11 @@ if ! tmux has-session -t "$SESSION" 2>/dev/null; then
 	tmux new-session -d -s "$SESSION" -n Obsidian -c "$ROOT"
   tmux send-keys -t "$SESSION":Obsidian 'work' C-m
   tmux send-keys -t "$SESSION":Obsidian ':Obsidian today' C-m
+  tmux split-window -v -t "$SESSION":Obsidian
+  tmux split-window -h -t "$SESSION":Obsidian.1 -p 75
+
+  tmux send-keys -t "$SESSION":Obsidian.1 'tock -m -s -c' C-m
+  tmux send-keys -t "$SESSION":Obsidian.2 'weather' C-m
 
 	tmux new-window -t "$SESSION" -n Prog -c "$CWD"
 	tmux send-keys -t "$SESSION":Prog 'nvim' C-m
