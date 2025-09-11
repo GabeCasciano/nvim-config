5194dd26facf1cc56fb3bd0ad30b3294a5b471e7
GabeCasciano <gabecasciano@gmail.com>
2025-09-11 09:18:05 -0400

    updated install for weather

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-cp ~/.config//nvim/start_work.sh ~/.local/bin/start_work.sh
+cp ~/.config/nvim/start_work.sh ~/.local/bin/start_work.sh
+cp ~/.config/nvim/weather.sh ~/.local/bin/weather.sh
 ln -s ~/.local/bin/start_work.sh ~/.local/bin/start_work
+ln -s ~/.local/bin/weather.sh ~/.local/bin/weather
 
